c2f5cc01e29fe07ec4a220b1d206c8480806661b
Zack <zackweeden@aol.com>
2017-05-01 01:57:35 -0400

    Changed some of the color codes and ansi

--- colorCodes.py
+++ colorCodes.py
@@ -1,9 +1,16 @@
 colorCodes = {
-	'RED': "\033[1;31m",
-	'BLUE': "\033[1;34m",
-	'CYAN': "\033[1;36m",
-	'PURPLE': '\033[95m',
-	'DARKCYAN': '\033[36m',
-	'YELLOW': '\033[93m'
+    'red':'\033[31m',
+    'orange':'\033[33m',
+    'blue':'\033[34m',
+    'purple':'\033[35m',
+    'cyan':'\033[36m',
+    'lightgrey':'\033[37m',
+    'darkgrey':'\033[90m',
+    'lightred':'\033[91m',
+    'lightgreen':'\033[92m',
+    'yellow':'\033[93m',
+    'lightblue':'\033[94m',
+    'pink':'\033[95m',
+    'lightcyan':'\033[96m'
 }
 COLOR_END = '\033[0m'
